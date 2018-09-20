747119f75c4e70f62c69d99fa535e0347cda0d7d
richardjtelford <richard.telford@bio.uib.no>
2018-09-20 19:18:44 +0200

    continue

--- R/insist.R
+++ R/insist.R
@@ -2,7 +2,8 @@
 #' @description Insists a object meets a given criteria
 #' @param . object
 #' @param test logical test, expression
-#' @details Tests if the object meets the citeria. If it does, the object is returned. If not, an error is thrown. Test should be an expression that refers to the object as . 
+#' @param continue if true prints error, if false throws error
+#' @details Tests if the object meets the citeria. If it does, the object is returned. If not, an error is thrown unless continue is true, in which case the error is returned. Test should be an expression that refers to the object as . 
 #' @return the object 
 #' @examples
 #' require(magrittr)
@@ -10,9 +11,15 @@
 #' pi %>% insist(length(.) == 1)
 #' @export
 
-insist <- function(., test){
+insist <- function(., test, continue = TRUE){
+  
   if(!eval(substitute(test), envir = parent.frame())){
-    stop(deparse(substitute(test)), " is not TRUE: \n. =  ", .)
+    error <- paste0(deparse(substitute(test)), " is not TRUE: \n. =  ", .)
+    if(continue){
+      return(error)
+    }else{
+      stop(error)
+    }
   } 
   return(.)
 }
