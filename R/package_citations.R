612b096a0770782be56f8bb325abf05e44aafbe4
richardjtelford <richard.telford@bio.uib.no>
2018-11-24 13:07:37 +0100

    namespace

--- R/package_citations.R
+++ R/package_citations.R
@@ -3,19 +3,23 @@
 #' @param packages vector of packages for which citations are required
 #' @param old_bib old bibtex file to copy
 #' @param new_bib new bibtex file
+#' @importFrom fs file_copy
+#' @importFrom utils citation
+#' @importFrom bibtex write.bib
+#' 
 #' @export
 
 package_citations <- function(packages, old_bib, new_bib){
   
   #copy original bib file
-  fs::file_copy(old_bib, new_bib, overwrite = TRUE)
+  file_copy(old_bib, new_bib, overwrite = TRUE)
   
   #R citation
   Rcite <- citation()
   Rcite$key = "R"
-  bibtex::write.bib(Rcite, new_bib, append = TRUE)
+  write.bib(Rcite, new_bib, append = TRUE)
   
   #package citation
-  bibtex::write.bib(packages, new_bib, append = TRUE)
+  write.bib(packages, new_bib, append = TRUE)
 }  
   
